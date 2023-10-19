b0603b41ad8592e63f0ed9bcbf9b93d7b49d91cf
Marcos Domingues <dfenixweb@gmail.com>
2023-10-19 10:36:33 -0300

    Epilif

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>O Pato voa, a galinha cisca, eu sou eu e jacaré é um bixo.</h1>;
+  return (
+    <h1>
+      O Pato voa, a galinha cisca, eu sou eu e jacaré é um bixo. E Pilif é o
+      cara!
+    </h1>
+  );
 }
 
 export default Home;
