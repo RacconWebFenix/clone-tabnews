0e059d440277a26a2b33f33a91f468a8db496cb8
Marcos Domingues <dfenixweb@gmail.com>
2023-10-23 08:13:55 -0300

    adiciona arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,6 @@
-import styles from "./style.module.css";
-
 function Home() {
-  return (
-    <div className={styles.container}>
-    <div>
-      <h1 className={styles.h1}>Clique no quadrado vermelho, siga os passos e ganhe uma nova experiência em desenvolvimento web incrível!!! Dica pressione F12</h1>
-    </div>
-    <div className={styles.tricks}>
-
-      <div onClick={() => alert("Parabéns. Você clicou! Você conseguiu")} className={styles.btn} />
-
-    </div>
-    </div>
-  );
+  
+  return <h1>Texto inical</h1>;
 }
 
 export default Home;
