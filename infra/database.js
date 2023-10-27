caea9956892ead95508fb6ab142cfd69f365bdc0
Marcos Domingues <dfenixweb@gmail.com>
2023-10-27 08:40:32 -0300

    in progress

--- infra/database.js
+++ infra/database.js
@@ -2,10 +2,10 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.DBHOST,
-    user: process.env.DBUSER,
-    database: process.env.DBNAME,
-    password: process.env.DBPASSWORD,
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
   const result = await client.query(queryObject);
